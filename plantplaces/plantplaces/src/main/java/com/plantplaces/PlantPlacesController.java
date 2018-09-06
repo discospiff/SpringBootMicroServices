e4faa2609ab8f995974b82d4c6cb527e272cd43c
Administrator <Administrator@DESKTOP-099UQRG>
2018-09-05 20:12:32 -0400

    Add Javadoc, add default /  endpoint

--- plantplaces/plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -6,11 +6,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PlantPlacesController {
-	
+
+	/**
+	 * Handle the /start endpoint.
+	 * @return
+	 */
 	@RequestMapping("/start")
 	public String start() {
 		return "start";
 	}
 
+	@RequestMapping("/")
+	public String index() {
+		return "start";
+	}
 	
 }
