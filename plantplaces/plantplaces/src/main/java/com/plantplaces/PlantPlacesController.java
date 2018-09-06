16b69d72d9e28e2891f17f73c69814ec2a73bb3b
Administrator <Administrator@DESKTOP-099UQRG>
2018-09-05 20:17:24 -0400

    Update / endpoint

--- plantplaces/plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -16,6 +16,10 @@ public class PlantPlacesController {
 		return "start";
 	}
 
+	/**
+	 * Handle the / endpoint
+	 * @return
+	 */
 	@RequestMapping("/")
 	public String index() {
 		return "start";
