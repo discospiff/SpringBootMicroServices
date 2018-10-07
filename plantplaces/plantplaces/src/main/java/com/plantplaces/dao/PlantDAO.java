6971e84d579c14a7ea4e96a832fcfbbf56d6de27
Administrator <Administrator@DESKTOP-099UQRG>
2018-10-07 15:59:46 -0400

    Parse raw JSON data to object

--- plantplaces/plantplaces/src/main/java/com/plantplaces/dao/PlantDAO.java
+++ plantplaces/plantplaces/src/main/java/com/plantplaces/dao/PlantDAO.java
@@ -2,6 +2,8 @@ package com.plantplaces.dao;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -19,7 +21,34 @@ public class PlantDAO implements IPlantDAO {
 	public List<PlantDTO> fetch(String searchFilter) throws Exception {
 		List<PlantDTO> allPlants = new ArrayList<PlantDTO>();
 		
-		networkDAO.request("http://plantplaces.com/perl/mobile/viewplantsjson.pl?Combined_Name=Oak");
+		String rawJson = networkDAO.request("http://plantplaces.com/perl/mobile/viewplantsjson.pl?Combined_Name=Oak");
+		
+		JSONObject root = new JSONObject(rawJson);
+		
+		JSONArray plants = root.getJSONArray("plants");
+		
+		for(int i = 0; i < plants.length(); i++) {
+			// the JSON data
+			JSONObject jsonPlant = plants.getJSONObject(i);
+			// Plant object that we will populate from JSON data.
+			PlantDTO plant = new PlantDTO();
+			int guid = jsonPlant.getInt("id");
+			String genus = jsonPlant.getString("genus");
+			String species = jsonPlant.getString("species");
+			String cultivar = jsonPlant.getString("cultivar");
+			String common = jsonPlant.getString("common");
+			
+			// populate our DTO with this information,.
+			plant.setGuid(guid);
+			plant.setGenus(genus);
+			plant.setCommon(common);
+			plant.setCultivar(cultivar);
+			plant.setSpecies(species);
+			
+			// add the populated plant to our collection.
+			allPlants.add(plant);
+			
+		}
 		
 		return allPlants;
 	}
