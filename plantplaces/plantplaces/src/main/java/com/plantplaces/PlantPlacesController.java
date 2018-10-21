db63b383196152d81e95cf7b22d46a086d00e15a
Administrator <Administrator@DESKTOP-099UQRG>
2018-10-21 14:52:11 -0400

    Remove hardcoding; push data from form to database.

--- plantplaces/plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
+++ plantplaces/plantplaces/src/main/java/com/plantplaces/PlantPlacesController.java
@@ -34,11 +34,6 @@ public class PlantPlacesController {
 
 	@RequestMapping(value="/savespecimen")
 	public String saveSpecimen(SpecimenDTO specimenDTO) {
-		specimenDTO.setDescription("Beautiful Leaf Color");
-		specimenDTO.setPlantId(83);
-		specimenDTO.setLatitude("38.74");
-		specimenDTO.setLongitude("-85.51");
-		specimenDTO.setPlantName("Quercus rubrum Red Oak");
 		try {
 			specimenService.save(specimenDTO);
 		} catch (Exception e) {
